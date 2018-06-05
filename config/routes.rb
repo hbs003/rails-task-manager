93998df487ef06a881933b2bdd12231f9a2ac26d
Henrique Silva <hbs003@hotmail.com>
2018-06-05 15:15:56 +0200

    01 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # What route convention @dhh did choose?
+
+  # Generic route:
+  # VERB PATH, to: 'CONTROLLER#ACTION'
+
+  resources :tasks
+  # get "tasks", to: "tasks#index"
+  # get "tasks/new", to: "tasks#new"
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit"
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+
+
+
+
+
+
+
 end
